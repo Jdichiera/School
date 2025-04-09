dc08fe7a4337d0ec497781b4e0cae21f9cd3384d
Jeramy Dichiera <jeramy.dichiera@gmail.com>
2025-04-08 21:09:51 -0400

    Add functionality

--- Python/LinkedList/linked_list.py
+++ Python/LinkedList/linked_list.py
@@ -9,4 +9,44 @@ class Linked_List:
             self.tail = new_node
         else:
             self.tail.next = new_node
-            self.tail - new_node
+            self.tail = new_node
+
+    def prepend(self, new_node):
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+    def insert_after(self, current_node, new_node):
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        elif current_node is self.tail:
+            self.tail.next = new_node
+            self.tail = new_node
+        else:
+            new_node.next = current_node.next
+            current_node.next = new_node
+
+    def remove_after(self, current_node):
+        # if the current node is null but there is a head, then we are removing the first node
+        # set the incoming node to the heads nextnode
+        # if the incming node is also null then we know that we have removed the only node and
+        # we can set the tail to none as well
+        if (current_node == None) and (self.head != None):
+            succeeding_node = self.head.next
+            self_head = succeeding_node
+            if succeeding_node == None:
+                self.tail = None
+        # If the current node is not null then it must be a node in the list
+        # We want to set the incoming node to the current nodes next next - so two nodes ahead
+        # this is because we will be removing the node after the current node
+        # if the incoming node is null then that means the node we are removing is the tail
+        # and we need to set the tail to the current node
+        elif current_node != None:
+            succeeding_node = current_node.next.next
+            current_node.next = succeeding_node
+            if succeeding_node == None:
+                self.tail = current_node
--- Python/LinkedList/main.py
+++ Python/LinkedList/main.py
@@ -1,15 +1,41 @@
 from node import Node
 from linked_list import Linked_List
 
-def main():
-    node1 = Node(25)
-    node1.print_data()
-    node2 = Node(50)
-    node2.print_data()
-    node1.next = node2
-    node1.print_next()
-    node2.print_next()
+
+num_list = Linked_List()
+
+node_a = Node(66)
+node_b = Node(99)
+node_c = Node(44)
+node_d = Node(95)
+node_e = Node(42)
+node_f = Node(17)
+
+num_list.append(node_b)   # Add 99
+num_list.append(node_c)   # Add 44, make the tail
+num_list.append(node_e)   # Add 42, make the tail
+
+num_list.prepend(node_a)  # Add 66, make the head
+
+num_list.insert_after(node_c, node_d)  # Insert 95 after 44
+num_list.insert_after(node_e, node_f)  # Insert 17 after tail (42)
+
+# Output list
+print('List after adding nodes:', end=' ')
+node = num_list.head
+while node != None:
+    print(node.data, end=' ')
+    node = node.next
+print()
+
+num_list.remove_after(node_c)   # Remove the tail (17)
+num_list.remove_after(None)     # Remove the head (66)
 
 
-if __name__ == "__main__":
-    main()
+# Output final list
+print('List after removing nodes:', end=' ')
+node = num_list.head
+while node != None:
+    print(node.data, end=' ')
+    node = node.next
+print()
